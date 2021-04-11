389ce1a652c087812e4b9e718408b5159bc5f13b
root <root@ip-172-31-58-81.ec2.internal>
2021-04-11 21:02:18 +0000

    latest commit

--- flask/flaskblog/__init__.py
+++ flask/flaskblog/__init__.py
@@ -1,28 +1,26 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flaskblog.config import Config
 
-os.environ['username2'] = 'adamtesting82@gmail.com'
-os.environ['password2'] = 'thisisfortesting'
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'f229723ac8d63f2af82f5a9a150d59e6'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-print(os.environ.get('username2'))
-print(os.environ.get('password2'))
-app.config['MAIL_USERNAME'] = os.environ.get('username2')
-app.config['MAIL_PASSWORD'] = os.environ.get('password2')
+
+
 mail = Mail(app)
 
-from flaskblog import routes
+from flaskblog.users.routes import users
+from flaskblog.posts.routes import posts
+from flaskblog.main.routes import main
+
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
---
+++ flask/flaskblog/config.py
@@ -0,0 +1,15 @@
+import os
+
+os.environ['EMAIL_USER'] = 'adamtesting82@gmail.com'
+os.environ['EMAIL_PASS'] = 'thisisfortesting'
+os.environ['SECRET_KEY'] = 'f229723ac8d63f2af82f5a9a150d59e6'
+os.environ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+class Config():
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
